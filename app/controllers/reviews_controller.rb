ccf98b8e91d0557820380c412833b0d0d5025002
Mariam-RM <mariam.majeed.kes@gmail.com>
2019-01-20 22:00:23 +0000

    completed all required features except for delete which is deleting from database but render isnt reflecting that

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
 
   def create
     @review = Review.new(review_params)
@@ -13,57 +14,16 @@ class ReviewsController < ApplicationController
 
   end
 
+  def destroy
+    r = Review.find(params[:id])
+    r.destroy
+    redirect_to "/products/#{params[:product_id]}"
+  end
+
+
   private
     def review_params
       params[:product].require(:reviews).permit(:rating, :description)
     end
 
- #  def create
- #    @review = Review.new(review_params)
- #    @review.user_id = session[:user_id]
-
- #    if @review.valid?
-
- #     @review.save!
- #      # redirect_to '/', notice: 'Your review has been added'
- #    else
-
- #      redirect_to '/login', notice: 'your review has not been added'
-
- #    end
-
-
- # end
-
-
- #  private
-
- #  # def create_review
- #  #   review = Review.new (
- #  #     product_id: params[:product_id],
- #  #     user_id: current_user,
- #  #     description: "I like this" ,
- #  #     rating: "5"
- #  #   )
- #  # end
-
- #  #   review
- #  # end
- #    def review_params
- #      params[:product].require(:reviews).permit(:product_id,:description, :rating)
- #    end
 end
-
-
-  # order = Order.new(
-  #     email: params[:stripeEmail],
-  #     total_cents: cart_subtotal_cents,
-  #     stripe_charge_id: stripe_charge.id, # returned by stripe
-  #   )
-
-# params.require(:user).permit(:name, :email, :password, :password_confirmation)
-
-  # t.integer  "product_id"
-  #   t.integer  "user_id"
-  #   t.text     "description"
-  #   t.integer  "rating"
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
 
 
